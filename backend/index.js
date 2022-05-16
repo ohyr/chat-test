593a409fe9b6d6a4b3d44183448fadac0a36282f
오용록 <ohyr9685@gmail.com>
2022-05-17 01:14:59 +0900

    Init backend

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const port = 9997;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome chat server</h1>");
+});
+
+io.on("connection", (socket) => {
+  console.log("connected!");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected!");
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
